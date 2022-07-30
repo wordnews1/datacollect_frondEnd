801ddb85211d3ca6dce9936758dcd55867b2ed52
franck <franck.tchokotcheu@angloenterprises.com>
2022-07-30 11:52:22 +0200

    edit path

--- src/plugins/constants.js
+++ src/plugins/constants.js
@@ -1,13 +1,13 @@
 const server_url = process.env.VUE_APP_SERVER_URL;
 const client_url = process.env.VUE_APP_CLIENT_URL;
 const keycloak_url = process.env.VUE_APP_KEYCLOAK_URL;
-const reporting_url =  process.env.VUE_APP_REPORTING_URL;
+//const reporting_url =  process.env.VUE_APP_REPORTING_URL;
 
 
 const mtc_port = process.env.VUE_APP_MTC_PORT;
 const home_port =  process.env.VUE_APP_HOME_PORT;
 const keycloak_port =  process.env.VUE_APP_KEYCLOAK_PORT;
-const reporting_port =  process.env.VUE_APP_REPORTING_PORT;
+//const reporting_port =  process.env.VUE_APP_REPORTING_PORT;
 
 
 const resource_url = "http://"+server_url+":"+ mtc_port +"/api/v1/";
@@ -19,7 +19,7 @@ const resource_home = "http://"+server_url+":"+ home_port;
 const resource_redirect = "http://"+keycloak_url+":"+ keycloak_port;
 const resource_url_auth = "http://"+keycloak_url+":"+ keycloak_port +"/auth/";
 const resource_url_users = "http://"+keycloak_url+":"+ keycloak_port +"/auth/admin/master/console/#/realms/mtckeycloak/users";
-const reporting_link = "http://"+reporting_url+":"+ reporting_port +"/art";
+//const reporting_link = "http://"+reporting_url+":"+ reporting_port +"/art";
 
 
 
